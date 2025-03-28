cf8e40cceec1a99342d9b647663912fd8af51edb
Raúl Belchí <raulbelchi19@gmail.com>
2025-03-28 19:38:10 +0100

    Login y register
    He subido las páginas que serán para el login y el registro de usuarios en la web.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,20 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Title from './components/Title'
 import Info from './components/info'
-import Post from './components/Post'
 import PostPage from './components/PostPage'
 
 function App() {
   return (
     <div className='flex flex-col items-center m-o p-0'>
       <Header />
+      
       <PostPage/>
-      {/*
+      
+    {/*
       <Title />
       <Info />
       */}
+      
       <Footer />
     </div>
   )
